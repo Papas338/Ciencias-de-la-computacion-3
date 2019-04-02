6e2b56816f67193c1288410c6d31d23191be7f09
Daniel <danielvargas999@gmail.com>
2019-04-02 16:33:28 -0500

    Creando clase Libro

---
+++ libro.py
@@ -0,0 +1,11 @@
+class Libro:
+    def __init__(self,titulo,autor):
+        self.titulo = titulo
+        self.autor = autor
+    def getAutor(self):
+        return self.autor
+    def getTitulo(self):
+        return self.titulo
+
+if __name__ == '__main__':
+    print("x")
