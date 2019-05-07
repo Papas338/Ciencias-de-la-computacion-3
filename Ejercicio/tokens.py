96c36b52fb0943384684d3c66361df32cbabb3d7
Daniel <danielvargas999@gmail.com>
2019-05-07 17:18:52 -0500

    Tokens con expresiones regulares

---
+++ Ejercicio/tokens.py
@@ -0,0 +1,3 @@
+x = re.findall("[a-z][a-z]*", "51 3 20 13 2 3 + 2 platano f + - % *")
+x = re.findall("[+-/*]", "51 3 20 13 2 3 + 2 platano f + - % *")
+x = re.findall("\d+", "51 3 20 13 2 3 + 2 platano f")
