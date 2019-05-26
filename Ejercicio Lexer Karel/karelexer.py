afd3bb66bc5e6af1a2b444066577bd08f8a98c22
Papas338 <daniel.r.97@hotmail.com>
2019-05-25 19:56:38 -0500

    Subiendo lexer karel con aun mas tokens

--- Ejercicio Lexer Karel/karelexer.py
+++ Ejercicio Lexer Karel/karelexer.py
@@ -3,6 +3,11 @@ import ply.lex as lex
 tokens = ['VACIA','IDENTIFICADOR','NUMBER','CADENA','LETRA','DIGITO',"MIENTRAS","HACER","NO","Y","O"]
 instrucciones = ["apagate","gira-izquierda","avanza","coge-zumbador","deja-zumbador",
 "sal-de-instruccion","y","o"]
+funcionesBooleanas = ["frente-libre","frente-bloqueado","izquierda-libre","izquierda-bloqueada","derecha-libre",
+"derecha-bloqueada","junto-a-zumbador","no-junto-a-zumbador","algun-zumbador-en-la mochila",
+"ningun-zumbador-en-la mochila","orientado-al-norte","orientado-al-sur",
+"orientado-al-este","orientado-al-oeste","no-orientado-al-norte","no-orientado-al-sur",
+"no-orientado-al-este","no-orientado-al-oeste"]
 
 t_VACIA = r''
 t_IDENTIFICADOR = r'[A-Za-z][A-Za-Z0-9-]*'
