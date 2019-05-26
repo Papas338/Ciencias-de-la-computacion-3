bef172a85111e4db6c3b7e4545c8e5079bcda7ca
Daniel Vargas <danielvargas999@gmail.com>
2019-05-25 19:52:58 -0500

    Subiendo lexer karel con mas tokens

--- Ejercicio Lexer Karel/karelexer.py
+++ Ejercicio Lexer Karel/karelexer.py
@@ -1,11 +1,19 @@
 import ply.lex as lex
-tokens = ['NAME','NUMBER']
 
-"apagate","gira-izquierda","avanza","coge-zumbador","deja-zumbador",
-"sal-de-instruccion","y"
+tokens = ['VACIA','IDENTIFICADOR','NUMBER','CADENA','LETRA','DIGITO',"MIENTRAS","HACER","NO","Y","O"]
+instrucciones = ["apagate","gira-izquierda","avanza","coge-zumbador","deja-zumbador",
+"sal-de-instruccion","y","o"]
 
-t_identificador = r'[A-Za-z][A-Za-Z0-9-]*'
-t_cadena = r'\".*'
+t_VACIA = r''
+t_IDENTIFICADOR = r'[A-Za-z][A-Za-Z0-9-]*'
+t_CADENA = r'\".*'
+t_LETRA = r'[A-Za-z]'
+t_DIGITO = r'[0-9]'
+t_MIENTRAS = r'mientras'
+t_HACER = r'hacer'
+t_NO = r'no'
+t_Y = r'y'
+t_O = r'o'
 
 
 def t_NUMBER(t):
