6cc10d1dffba1f2eefdef05d20fcd8e225081201
Papas338 <daniel.r.97@hotmail.com>
2019-05-25 21:24:55 -0500

    Definiciones de booleanos

--- Ejercicio Lexer Karel/karelexer.py
+++ Ejercicio Lexer Karel/karelexer.py
@@ -4,8 +4,8 @@ tokens = ['VACIA','IDENTIFICADOR','NUMBER','CADENA','LETRA','DIGITO',"MIENTRAS",
 instrucciones = ["apagate","gira-izquierda","avanza","coge-zumbador","deja-zumbador",
 "sal-de-instruccion","y","o"]
 funcionesBooleanas = ["frente-libre","frente-bloqueado","izquierda-libre","izquierda-bloqueada","derecha-libre",
-"derecha-bloqueada","junto-a-zumbador","no-junto-a-zumbador","algun-zumbador-en-la mochila",
-"ningun-zumbador-en-la mochila","orientado-al-norte","orientado-al-sur",
+"derecha-bloqueada","junto-a-zumbador","no-junto-a-zumbador","algun-zumbador-en-la-mochila",
+"ningun-zumbador-en-la-mochila","orientado-al-norte","orientado-al-sur",
 "orientado-al-este","orientado-al-oeste","no-orientado-al-norte","no-orientado-al-sur",
 "no-orientado-al-este","no-orientado-al-oeste"]
 
@@ -19,6 +19,24 @@ t_HACER = r'hacer'
 t_NO = r'no'
 t_Y = r'y'
 t_O = r'o'
+t_FLIBRE = r'frente-libre'
+t_FBLOQUADO = r'frente-bloqueado'
+t_ILIBRE = r'izquierda-libre'
+t_IBLOQUEADA = r'izquierda-bloqueada'
+t_DLIBRE = r'derecha-libre'
+t_DBLOQUEADA = r'derecha-bloqueada'
+t_JZUMBADOR = r'junto-a-zumbador'
+t_NJZUMBADOR = r'no-junto-a-zumbador'
+t_MOZUMBADOR = r'algun-zumbador-en-la-mochila'
+t_NOMOZUMBADOR = r'ningun-zumbador-en-la-mochila'
+t_ORNORTE = r'orientado-al-norte'
+t_ORSUR = r'orientado-al-sur'
+t_OROESTE = r'orientado-al-oeste'
+t_ORESTE = r'orientado-al-este'
+t_NORNORTE = r'no-orientado-al-norte'
+t_NORSUR = r'no-orientado-al-sur'
+t_NOROESTE = r'no-orientado-al-oeste'
+t_NORESTE = r'no-orientado-al-este'
 
 
 def t_NUMBER(t):
