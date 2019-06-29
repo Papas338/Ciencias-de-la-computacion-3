251748d9e1a09b5d495c0b97d354c2885a3c46cf
Papas338 <daniel.r.97@hotmail.com>
2019-06-29 12:40:22 -0500

    Agregados archivos de generador de reportes

---
+++ Ejercicio Generador/srcgen/Telefono.java
@@ -0,0 +1,22 @@
+public class Telefono {
+
+       private String indicador;
+       private String numero;
+
+    public String getIndicador(){
+      return this.indicador;
+    }
+    public String getNumero(){
+      return this.numero;
+    }
+
+      public void setIndicador(String value){
+        this.indicador = value;
+      }
+
+      public void setNumero(String value){
+        this.numero = value;
+      }
+
+
+}
