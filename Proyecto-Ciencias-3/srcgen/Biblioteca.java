163d0405aa04c12184fbc2a51a64d7d4552933a5
Papas338 <daniel.r.97@hotmail.com>
2019-07-23 06:35:16 -0500

    Agregado proyecto final

---
+++ Proyecto-Ciencias-3/srcgen/Biblioteca.java
@@ -0,0 +1,34 @@
+public class Biblioteca {
+
+       private String nombre;
+       private String direccion;
+       private String telefono;
+	
+
+public Biblioteca(){
+}
+  
+    public string getNombre(){
+      return this.nombre;
+    }
+    public string getDireccion(){
+      return this.direccion;
+    }
+    public string getTelefono(){
+      return this.telefono;
+    }
+
+      public void setNombre(string value){
+        this.nombre = value;
+      }
+
+      public void setDireccion(string value){
+        this.direccion = value;
+      }
+
+      public void setTelefono(string value){
+        this.telefono = value;
+      }
+
+
+}
---
+++ Proyecto-Ciencias-3/srcgen/Libro.java
@@ -0,0 +1,42 @@
+public class Libro {
+
+       private String id;
+       private String nombre;
+       private String genero;
+       private String accion;
+	
+
+public Libro(){
+}
+  
+    public string getId(){
+      return this.id;
+    }
+    public string getNombre(){
+      return this.nombre;
+    }
+    public string getGenero(){
+      return this.genero;
+    }
+    public string getAccion(){
+      return this.accion;
+    }
+
+      public void setId(string value){
+        this.id = value;
+      }
+
+      public void setNombre(string value){
+        this.nombre = value;
+      }
+
+      public void setGenero(string value){
+        this.genero = value;
+      }
+
+      public void setAccion(string value){
+        this.accion = value;
+      }
+
+
+}
